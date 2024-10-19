4bee2a5cdf9c9683fa80c95f60776a38520cbc20
jaekim3220 <jehyunkim9@gmail.com>
2024-10-19 11:27:41 +0900

    breakpoin 설명 추가

--- src/main/java/com/quiz/lesson06/Lesson06Quiz01Controller.java
+++ src/main/java/com/quiz/lesson06/Lesson06Quiz01Controller.java
@@ -1,6 +1,8 @@
 package com.quiz.lesson06;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -51,7 +53,7 @@ public class Lesson06Quiz01Controller {
 	*/
 	
 	// 북마크 추가 화면
-	// localhost/lesson06/quiz01/add-bookmark-view
+	// http:localhost:80/lesson06/quiz01/add-bookmark-view
 	@GetMapping("/add-bookmark-view")
 	public String addBookmarkVeiw() {
 		return "lesson06/addBookmark";
@@ -61,11 +63,22 @@ public class Lesson06Quiz01Controller {
 	// 북마크 추가 진행 => AJAX 요청
 	@ResponseBody
 	@PostMapping("/add-bookmark")
-	public String addBookmark(
+	public Map<String, Object> addBookmark(
 			@RequestParam("name") String name,
 			@RequestParam("url") String url) {
 		
-		return "성공";
+		// DB INSERT ★★★ breakpoint 1 ★★★
+		// Model은 HTML일 경우 사용(@ResponseBody일 경우 Model 사용 불가)
+		bookmarkBO.addBookmark(name, url);
+		
+		// 성공 여부 JSON String ★★★ breakpoint 2 ★★★
+		// "{"code":200, "result":"성공"}" 형식
+		// 사용하기 위해서는 JSON을 Dictionary 형식으로 바꿀 필요가 있음
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 200);
+		result.put("result", "성공");
+		
+		return result;
 	}
 	
 	
@@ -74,10 +87,10 @@ public class Lesson06Quiz01Controller {
 	입력 성공 이후에 리스트 화면으로 이동하세요.
 	*/
 	
-	// localhost/lesson06/quiz01/after-add-bookmark
-	@GetMapping("/after-add-bookmark")
-	public String afterAddBookmark(Model model) {
-
+	// 즐겨찾기 목록
+	// http:localhost:80/lesson06/quiz01/bookmark-list-view
+	@GetMapping("/bookmark-list-view")
+	public String bookMarkListView(Model model) {
 		
 		// DB SELECT Service(BO)영역
 		List<Bookmark> bookmarkList = bookmarkBO.getBookmarkList();
@@ -86,6 +99,7 @@ public class Lesson06Quiz01Controller {
 		// Controller가 Model에 데이터를 삽입
 		// HTML(VIEW)가 Model에서 꺼내서 사용
 		model.addAttribute("bookmarkList", bookmarkList);
+		
 		return "lesson06/bookmarkList";
 	}
 	
