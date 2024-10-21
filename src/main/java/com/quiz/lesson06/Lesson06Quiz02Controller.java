e378ca06e40ed5b3e8f28640f83a4842a7512041
jaekim3220 <jehyunkim9@gmail.com>
2024-10-21 23:45:31 +0900

    AJAX 통신 `AJAX로 중복확인` 복습을 위한 기본 뼈대 생성 반드시 복습할 것

--- src/main/java/com/quiz/lesson06/Lesson06Quiz02Controller.java
+++ src/main/java/com/quiz/lesson06/Lesson06Quiz02Controller.java
@@ -1,16 +1,20 @@
 package com.quiz.lesson06;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.quiz.lesson06.bo.BookmarkBO;
+import com.quiz.lesson06.domain.Bookmark;
 
 /*
 DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> Repository영역(Mapper) <--> DB영역 
@@ -29,6 +33,50 @@ DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> 
 @Controller
 @RequestMapping("/lesson06/quiz02")
 public class Lesson06Quiz02Controller {
+	
+	// 어노테이션(Annotation)
+	@Autowired // DI(Dependency Injection) : 의존성 주입
+	private BookmarkBO bookmarkBO1;
+	
+	/* 6-1 연습문제 : AJAX 통신으로 서버 요청*/
+	
+	
+	/*
+	1. AJAX 통신으로 submit 대신 서버 요청
+	bookmark 테이블을 이용하세요.
+	입력 화면을 통해서 새로운 즐겨 찾기 목록을 추가하세요.
+	모든 칸에 입력되었는지 validation 코드를 추가하세요.
+	http 또는 https 프로토콜까지 모두 입력되었는지 validation 코드를 추가하세요.
+	form submit 을 사용하지 않고 ajax 를 이용해서 수행하세요.
+	*/
+	
+	// 북마크 추가 화면
+	// http:localhost:80/lesson06/quiz02/add-bookmark-view1
+	@GetMapping("/add-bookmark-view1")
+	public String addBookmarkVeiw1() {
+		return "lesson06/addBookmark";
+	}
+	
+	/*
+	2. 결과 페이지 이동
+	입력 성공 이후에 리스트 화면으로 이동하세요.
+	*/
+	
+	// 즐겨찾기 목록
+	// http:localhost:80/lesson06/quiz01/bookmark-list-view
+	@GetMapping("/bookmark-list-view1")
+	public String bookMarkListView(Model model) {
+		
+		// DB SELECT Service(BO)영역
+		List<Bookmark> bookmarkList = bookmarkBO1.getBookmarkList();
+		
+		// MODEL 데이터 삽입
+		// Controller가 Model에 데이터를 삽입
+		// HTML(VIEW)가 Model에서 꺼내서 사용
+		 model.addAttribute("bookmarkList", bookmarkList);
+		
+		return "lesson06/bookmarkList";
+	}
 	
 	
 	/* 6-2 연습문제 : AJAX로 중복확인*/
